9ede8a7f1c1704d0bc9848f569dbaf1331d16d11
Sotatek-TuNguyen6 <tu.nguyen6@sotatek.com>
2024-11-21 19:33:37 +0700

    chore:add logic service user

--- apps/user-service/src/user/user.service.ts
+++ apps/user-service/src/user/user.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { PasswordService } from "../auth/password.service";
 import { UserServiceBase } from "./base/user.service.base";
+import { RabbitMQProducerService } from "src/rabbitmq/rabbitmq.producer.service";
 
 @Injectable()
 export class UserService extends UserServiceBase {
   constructor(
     protected readonly prisma: PrismaService,
-    protected readonly passwordService: PasswordService
+    protected readonly passwordService: PasswordService,
   ) {
     super(prisma, passwordService);
   }
