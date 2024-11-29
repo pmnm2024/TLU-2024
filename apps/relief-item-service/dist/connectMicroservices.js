3a7fdfa109963f60510da619435ec9adf0d7ba8b
pmnm2024 <devtu32@gmail.com>
2024-11-29 22:10:56 +0700

    Delete apps/relief-item-service directory

--- apps/relief-item-service/dist/connectMicroservices.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectMicroservices = connectMicroservices;
-const config_1 = require("@nestjs/config");
-async function connectMicroservices(app) {
-    const configService = app.get(config_1.ConfigService);
-}
-//# sourceMappingURL=connectMicroservices.js.map
