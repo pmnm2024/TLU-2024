7758e096ec350d01a75ed5e95a9a98bdc73cdd41
pmnm2024 <devtu32@gmail.com>
2024-12-01 04:55:02 +0700

    gửi mail cảm ơn khi quyên góp thành công

--- apps/donation-service/src/donation/donation.controller.ts
+++ apps/donation-service/src/donation/donation.controller.ts
@@ -2,11 +2,46 @@ import * as common from "@nestjs/common";
 import * as swagger from "@nestjs/swagger";
 import { DonationService } from "./donation.service";
 import { DonationControllerBase } from "./base/donation.controller.base";
-
+import { Public } from "src/decorators/public.decorator";
+import { DonationCreateInput } from "./base/DonationCreateInput";
+import { Donation } from "./base/Donation";
 @swagger.ApiTags("donations")
 @common.Controller("donations")
 export class DonationController extends DonationControllerBase {
   constructor(protected readonly service: DonationService) {
     super(service);
+  }
+  @Public()
+  @common.Post("/addDonation")
+  async addDonation(@common.Body() data: DonationCreateInput): Promise<Donation> {
+    try {
+      const payLoad = {
+        data: data,
+        select: {
+          accountNumber: true,
+          amount: true,
+          bank: true,
+          city: true,
+          createdAt: true,
+          description: true,
+          detailAddress: true,
+          district: true,
+          email: true,
+          fullName: true,
+          id: true,
+          paymentMethod: true,
+          phone: true,
+          status: true,
+          supportRequestTypeId: true,
+          supportRequestTypeName: true,
+          updatedAt: true,
+          userId: true,
+          ward: true,
+        }  
+      }
+      return await this.service.addDonation(payLoad)
+    } catch (error) {
+      throw error
+    }
   }
 }
