e7565368ac37a15e3853bc9531fee9a8b4f540f8
Sotatek-TuNguyen6 <tu.nguyen6@sotatek.com>
2024-12-01 22:17:14 +0700

    update

--- apps/mail-service/src/historySendMail/historySendMail.module.ts
+++ apps/mail-service/src/historySendMail/historySendMail.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { HistorySendMailModuleBase } from "./base/historySendMail.module.base";
 import { HistorySendMailService } from "./historySendMail.service";
 import { HistorySendMailController } from "./historySendMail.controller";
+import { MailModule } from "src/mail/mail.module";
 
 @Module({
-  imports: [HistorySendMailModuleBase],
+  imports: [HistorySendMailModuleBase, MailModule],
   controllers: [HistorySendMailController],
   providers: [HistorySendMailService],
   exports: [HistorySendMailService],
 })
-export class HistorySendMailModule {}
+export class HistorySendMailModule { }
--- apps/mail-service/src/historySendMail/historySendMail.service.ts
+++ apps/mail-service/src/historySendMail/historySendMail.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { HistorySendMailServiceBase } from "./base/historySendMail.service.base";
+import { MailService } from "src/mail/mail.service";
 
 @Injectable()
 export class HistorySendMailService extends HistorySendMailServiceBase {
-  constructor(protected readonly prisma: PrismaService) {
+  constructor(protected readonly prisma: PrismaService, protected readonly mailSerivce: MailService) {
     super(prisma);
+  }
+
+  async processSendMail(payload: any) {
+    try {
+      const { email, userId, description } = payload
+      const sendMail = await this.mailSerivce.sendMail({ to: email, subject: "reset password", html: description })
+      return sendMail;
+    } catch (error) {
+      throw error
+    }
   }
 }
