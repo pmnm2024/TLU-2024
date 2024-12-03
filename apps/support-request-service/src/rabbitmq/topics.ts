cbba66fbb79d68d67242fb88c6408b90ca91e4bb
pmnm2024 <devtu32@gmail.com>
2024-12-04 00:20:05 +0700

    Handle 2 message Support Request

--- apps/support-request-service/src/rabbitmq/topics.ts
+++ apps/support-request-service/src/rabbitmq/topics.ts
@@ -1,9 +1,5 @@
 export enum MyMessageBrokerTopics {
-
   Donate = "donate",
-  HandleSupportRequest = "handle.support.request",
-  HandleWarehouse = "handle.warehouse",
-  ResetPassword = "reset.password",
   HandleSupportRequest = "handle.support.request",
   HandleWarehouse = "handle.warehouse",
 }
