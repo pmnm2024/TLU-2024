cacffe8e13835e071aad2c8008149155323bd16c
pmnm2024 <devtu32@gmail.com>
2024-12-08 21:56:38 +0700

    update api gateway

---
+++ apps/api-gateway/my-gateway/plugins/logging/index.js
@@ -0,0 +1,47 @@
+
+module.exports = {
+  version: '1.0.0',
+
+  init: function (pluginContext) {
+
+    pluginContext.registerPolicy(require('./policies/log-policy'));
+
+    pluginContext.eventBus.on('hot-reload', function ({ type, newConfig }) {
+      console.log('hot-reload', type, newConfig);
+    });
+    pluginContext.eventBus.on('http-ready', function ({ httpServer }) {
+      console.log('http ready');
+    });
+    pluginContext.eventBus.on('https-ready', function ({ httpsServer }) {
+      console.log('https ready');
+    });
+    pluginContext.eventBus.on('admin-ready', function ({ adminServer }) {
+      console.log('admin ready');
+    });
+   
+  },
+
+  policies: ['log-policy'],
+
+  options: {
+    logLevel: 'info', 
+    logFile: 'logs/requests.log',  
+  },
+
+  schema: {
+    $id: 'express-gateway-plugin-logging',
+    type: 'object',
+    properties: {
+      logLevel: {
+        type: 'string',
+        enum: ['info', 'warn', 'error', 'debug'],
+        default: 'info',
+      },
+      logFile: {
+        type: 'string',
+        default: 'logs/requests.log',
+      },
+    },
+    required: ['logLevel'],  
+  },
+};
