0dafa95712c06bccbb9d4b80b237f582e29049a9
Sotatek-TuNguyen6 <tu.nguyen6@sotatek.com>
2024-12-09 17:46:31 +0700

    update

--- apps/mail-service/src/historySendMail/historySendMail.module.ts
+++ apps/mail-service/src/historySendMail/historySendMail.module.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) ATA_TLU.
+ *
+ * This source code is licensed under the GPL V3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
 import { Module } from "@nestjs/common";
 import { HistorySendMailModuleBase } from "./base/historySendMail.module.base";
 import { HistorySendMailService } from "./historySendMail.service";
--- apps/mail-service/src/rabbitmq/generateRabbitMQClientOptions.ts
+++ apps/mail-service/src/rabbitmq/generateRabbitMQClientOptions.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) ATA_TLU.
+ *
+ * This source code is licensed under the GPL V3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
 import { ConfigService } from "@nestjs/config";
 import { RmqOptions, Transport } from "@nestjs/microservices";
 
