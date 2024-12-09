c58a41a7f5a483056517672c82ca60c38ba83f88
Sotatek-TuNguyen6 <tu.nguyen6@sotatek.com>
2024-12-09 17:39:58 +0700

    update

--- apps/scripts/stop-all.sh
+++ apps/scripts/stop-all.sh
@@ -1,10 +1,4 @@
-/**
- * Copyright (c) ATA_TLU.
- *
- * This source code is licensed under the GPL V3 license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
+
 #!/bin/bash
 
 # Dừng từng service
